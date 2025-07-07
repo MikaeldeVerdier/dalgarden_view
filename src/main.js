b16cb2c5366cfabd05722f772cc1a3c7c5a2d10d
Mikael de Verdier <mikael.deverdier@gmail.com>
2025-07-07 14:08:08 +0200

    minor improvements

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ import { MarkersPlugin } from "@photo-sphere-viewer/markers-plugin";
 
 import { getNodesFromJSONFile } from "/src/nodes.js";
 
-getNodesFromJSONFile("/nodes head.json")
+getNodesFromJSONFile("/nodes_head.json")
     .then(nodes => {
         /* DEBUG: Gallery
         var gallery = [];
