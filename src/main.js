6a66b27d56b912b89516702bcb3d263255190489
Mikael de Verdier <mikael.deverdier@gmail.com>
2025-07-09 18:18:39 +0200

    fixed some markers and added some new ones; uncommented debug code; set preload to true

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { Viewer } from "@photo-sphere-viewer/core";
 import { VirtualTourPlugin } from "@photo-sphere-viewer/virtual-tour-plugin";
 // import "@photo-sphere-viewer/virtual-tour-plugin/index.css";
 
-// import { GalleryPlugin } from "@photo-sphere-viewer/gallery-plugin";
+import { GalleryPlugin } from "@photo-sphere-viewer/gallery-plugin";  // DEBUG: Gallery
 // import "@photo-sphere-viewer/gallery-plugin/index.css";
 
 import { MarkersPlugin } from "@photo-sphere-viewer/markers-plugin";
@@ -12,9 +12,9 @@ import { MarkersPlugin } from "@photo-sphere-viewer/markers-plugin";
 
 import { getNodesFromJSONFile } from "/src/nodes.js";
 
-getNodesFromJSONFile("./nodes_head.json")
+getNodesFromJSONFile("./nodes_head.json")  // getNodesFromJSONFile("./nodes_head0.json")  // DEBUG: 0 heading
     .then(nodes => {
-        /* DEBUG: Gallery
+        // /* DEBUG: Gallery
         var gallery = [];
         nodes.forEach(node => {
             gallery.push({
@@ -23,7 +23,7 @@ getNodesFromJSONFile("./nodes_head.json")
                 panorama: node.panorama
             })
         });
-        */
+        // */
 
         const viewer = new Viewer({
             plugins: [
@@ -33,11 +33,11 @@ getNodesFromJSONFile("./nodes_head.json")
                 [VirtualTourPlugin, {
                     positionMode: "manual",
                     nodes: nodes,
-                    // preload: true,
+                    preload: true,
                     showLinkTooltip: false,
                     renderMode: "2d",  // 3d doesn"t work at all (laggy and wrong position)
                 }],
-                /* DEBUG: Gallery
+                // /* DEBUG: Gallery
                 [GalleryPlugin, {
                     items: gallery,
                     // showNodeName: true,
@@ -46,7 +46,7 @@ getNodesFromJSONFile("./nodes_head.json")
                     // showNodePosition: true,
                     // showNodeLinks: true,
                 }],
-                */
+                // */
             ],
             container: document.querySelector("#viewer"),
             navbar: [
@@ -54,7 +54,7 @@ getNodesFromJSONFile("./nodes_head.json")
                 "move",
                 "markersList",
                 "caption",
-                // "gallery", DEBUG: Gallery
+                "gallery",  // DEBUG: Gallery
                 "fullscreen",
                 // "markers",
             ],
@@ -76,17 +76,17 @@ getNodesFromJSONFile("./nodes_head.json")
             //     });
         }, { once: true });
 
-        /* DEBUG: Click
+        // /* DEBUG: Click
         viewer.addEventListener("click", ({ data }) => {
             const centerCoordinate = offsetYawPitch(data.yaw, data.pitch, viewer);
             console.log(`${data.rightclick ? "right " : ""}clicked at yaw: ${centerCoordinate.yaw}, pitch: ${centerCoordinate.pitch}`);
             // console.log(`${data.rightclick ? "right " : ""}clicked at yaw: ${centerCoordinate.yaw * 180 / Math.PI} deg, pitch: ${centerCoordinate.pitch* 180 / Math.PI} deg`);
             // console.log(`${data.rightclick ? "right " : ""}clicked at textureX: ${data.textureX} pitch: ${data.textureY}`);
         });
-        */
+        // */
     })
 
-/* DEBUG: Click
+// /* DEBUG: Click
 function offsetYawPitch(yaw, pitch, viewer, offsetX = 0, offsetY = -16) {
     const vFov = viewer.defaultZoomLvl || 50 * (Math.PI / 180); // Default vertical field of view in radians
     const canvas = viewer.container;
@@ -103,4 +103,4 @@ function offsetYawPitch(yaw, pitch, viewer, offsetX = 0, offsetY = -16) {
         pitch: pitch + deltaPitch
     };
 }
-*/
+// */
